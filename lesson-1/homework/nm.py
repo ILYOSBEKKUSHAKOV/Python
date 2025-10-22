1bfb574c2ae1d0966a2fa793a1785b354d0853fa
ILYOSBEKKUSHAKOV <ilyasbekkoshaqov@gmail.com>
2025-10-22 23:58:38 +0300

    Create nm.py

---
+++ lesson-1/homework/nm.py
@@ -0,0 +1,28 @@
+#1 
+a=4
+P=(a+a)*2
+S=a**2
+print('P=',P)
+print('S=',S)
+
+#2
+import math
+
+d = 5
+C = d * math.pi
+print('The length of circle is =', C)
+
+#3
+a=123
+b=123
+c=a+b
+print('Their mean is = ', c)
+
+#4
+a=34
+b=23
+c=a+b
+d=a*b
+e=a**2
+f=b**2
+print('Answers:', c, d, e, f )
